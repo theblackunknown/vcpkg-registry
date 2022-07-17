0e42f3d696c4114206e1de4b78890b964826a438
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2022-07-17 23:13:26 +0200

    [mdl] update based on microsoft/vcpkg#24633 current work

--- ports/mdl/portfile.cmake
+++ ports/mdl/portfile.cmake
@@ -1,63 +1,45 @@
-
-# NOTE src/utilities.cmake:76 force x64 by defining BIT64=1
-vcpkg_fail_port_install(
-    ON_ARCH
-        x86
-)
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    # NOTE: Static library required on windows, it seems some shared library does not export symbol which prevents a .lib to be generated
-    # LINK : fatal error LNK1181: cannot open input file '..\..\Release\lib\LLVMDemangle.lib' [C:\devel\vcpkg\buildtrees\mdl\x64-windows-rel\src\mdl\jit\llvm\dist\lib\Support\LLVMSupport.vcxproj]
-
-    vcpkg_check_linkage(
-        ONLY_STATIC_LIBRARY
-    )
-
-    if((NOT VCPKG_BUILD_TYPE) OR (NOT VCPKG_BUILD_TYPE STREQUAL "release"))
-        message(STATUS "Note: ${PORT} only supports release build (FreeImage dependency does not compile with /MTd nor /MDd).")
-        set(VCPKG_BUILD_TYPE release)
-    endif()
-endif()
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO NVIDIA/MDL-SDK
-    REF d51e97f0019642f0cb1dfb7b67a4edaac2058f92
-    SHA512 585a58cd2f1dcf9c8e72dcfef57db8808b9ca4427e6914df3fee6f295d977b7f771531c3c275d54be3ec6c64fc1acfe7c7a4f6608bcf0831e11df6d9d5632ca3
+    REF d6c9a6560265025a30d16fcd9d664f830ab63109
+    SHA512 d6161a317ca0fd3cf8c782f058fc43765d611b5f6a8e82da736f5164a0e1829a46f75e376715fcb7cb9521406365aa88880ed44235b2bf63899affcc5bd54091
     HEAD_REF master
     PATCHES
         001-freeimage-from-vcpkg.patch
-        002-clang-7.0.x-and-above.patch
-        003-install-rules.patch
-        004-freeimage-disable-faxg3.patch
-        005-missing-std-includes.patch
-        006-missing-link-windows-crypt-libraries.patch
-        007-guard-nonexisting-targets.patch
-        008-disable-plugins.patch
+        002-install-rules.patch
+        003-freeimage-disable-faxg3.patch
+        004-missing-std-includes.patch
+        005-missing-link-windows-crypt-libraries.patch
+        006-guard-nonexisting-targets.patch
+        007-plugin-options.patch
+        008-build-static-llvm.patch
+        009-include-priority-vendored-llvm.patch
 )
 
-set(_MVSC_CRT_LINKAGE_OPTION)
-if(VCPKG_CRT_LINKAGE STREQUAL "static")
-    set(_MVSC_CRT_LINKAGE_OPTION -DMDL_MSVC_DYNAMIC_RUNTIME_EXAMPLES:BOOL=OFF)
-elseif(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
-    set(_MVSC_CRT_LINKAGE_OPTION -DMDL_MSVC_DYNAMIC_RUNTIME_EXAMPLES:BOOL=ON)
+string(COMPARE NOTEQUAL "${VCPKG_CRT_LINKAGE}" "static" _MVSC_CRT_LINKAGE_OPTION)
+
+vcpkg_find_acquire_program(PYTHON3)
+if(NOT DEFINED CLANG7)
+    message(FATAL_ERROR "Missing required variable CLANG7 from `vcpkg-tool-clang7`, please check your setup.")
 endif()
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_find_acquire_program(PYTHON3)
-    set(PATH_PYTHON ${PYTHON3})
-else()
-    # FIXME Python 3.6.9 is provided, but mdl requires 3.7 or 2.7
-    vcpkg_find_acquire_program(PYTHON2)
-    set(PATH_PYTHON ${PYTHON2})
-endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        dds MDL_BUILD_DDS_PLUGIN
+        freeimage MDL_BUILD_FREEIMAGE_PLUGIN
+    )
 
-vcpkg_find_acquire_program(CLANG)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/unofficial-mdl-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DMDL_LOG_DEPENDENCIES:BOOL=ON
+
+        -DMDL_MSVC_DYNAMIC_RUNTIME_EXAMPLES:BOOL=${_MVSC_CRT_LINKAGE_OPTION}
+
         -DMDL_ENABLE_CUDA_EXAMPLES:BOOL=OFF
         -DMDL_ENABLE_OPENGL_EXAMPLES:BOOL=OFF
         -DMDL_ENABLE_QT_EXAMPLES:BOOL=OFF
@@ -69,28 +51,23 @@ vcpkg_configure_cmake(
         -DMDL_BUILD_CORE_EXAMPLES:BOOL=OFF
         -DMDL_BUILD_ARNOLD_PLUGIN:BOOL=OFF
         
-        -DMDL_INSTALL_PLUGINS:BOOL=OFF
+        -Dpython_PATH:PATH=${PYTHON3}
+        -Dclang_PATH:PATH=${CLANG7}
 
-        -Dclang_PATH:PATH=${CLANG}
-        -Dpython_PATH:PATH=${PATH_PYTHON}
-
-        -DCMAKE_CXX_STANDARD:STRING=11
-        -DCMAKE_CXX_EXTENSIONS:STRING=ON
-
-        ${_MVSC_CRT_LINKAGE_OPTION}
-    OPTIONS_DEBUG
-        -DMDL_INSTALL_HEADERS:BOOL=OFF
+        ${FEATURE_OPTIONS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
-
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-mdl)
 vcpkg_copy_tools(
     TOOL_NAMES i18n mdlc mdlm
     AUTO_CLEAN
 )
 
-vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
